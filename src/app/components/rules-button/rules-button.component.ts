ca783178257bc74b7f926c353985fb12280eef81
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-01-26 12:59:44 +0200

    added component rules button

---
+++ src/app/components/rules-button/rules-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-button',
+  standalone: true,
+  templateUrl: './rules-button.component.html',
+  styleUrl: './rules-button.component.scss'
+})
+export class RulesButtonComponent {}
