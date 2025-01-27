bf3332b5a8d1ba1eedd3d8821d9ee6fbca171731
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-01-27 12:44:15 +0200

    added rules modal

---
+++ src/app/components/rules-modal/rules-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-modal',
+  standalone: true,
+  templateUrl: './rules-modal.component.html',
+  styleUrl: './rules-modal.component.scss'
+})
+export class RulesModalComponent {}
