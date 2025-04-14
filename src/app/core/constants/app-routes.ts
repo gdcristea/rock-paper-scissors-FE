3c80c121a9651e69376adf483347dc842a12db7e
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-04-14 19:07:45 +0300

    added routing, pages, moved the code from app component to game component

---
+++ src/app/core/constants/app-routes.ts
@@ -0,0 +1,12 @@
+/**
+ * Holds all the available routes
+ */
+export const AppRoutes = {
+  game: 'game',
+  login: 'login',
+  register: 'register',
+  error: 'error',
+  profile: 'profile',
+  leaderboard: 'leaderboard',
+  notFound: 'not-found'
+}
